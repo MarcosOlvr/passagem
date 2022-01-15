9baf42cf1a103ac769c142311b5f545d3b8854fa
Marcos <marquinhooliveira9968@outlook.com>
2022-01-15 19:42:36 -0300

    add noticias mvc

---
+++ Models/Noticias.cs
@@ -0,0 +1,12 @@
+namespace Passagem.Models
+{
+    public class Noticias
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Conteudo { get; set; }
+        public string Categoria { get; set; }
+        public DateTime CriadoEm { get; set; } = DateTime.Now;
+        public DateTime AtualizadoEm { get; set; } = DateTime.Now;
+    }
+}
