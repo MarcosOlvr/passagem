15434842b58270f9d5c132223dac705fa1beb82e
Marcos <marquinhooliveira9968@outlook.com>
2022-01-17 15:15:07 -0300

    correções

--- Models/Noticias.cs
+++ Models/Noticias.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Passagem.Models
 {
     public class Noticias
@@ -5,6 +7,8 @@ namespace Passagem.Models
         public int Id { get; set; }
         public string Titulo { get; set; }
         public string ResumoMateria { get; set; }
+
+        [Column(TypeName = "ntext")]
         public string Conteudo { get; set; }
         public string Categoria { get; set; }
         public DateTime CriadoEm { get; set; } = DateTime.Now;
