c081c6cca87ef9b32beec5ce2593f9369396e0eb
Marcos <marquinhooliveira9968@outlook.com>
2022-01-31 18:19:28 -0300

    fix estrutura // add user crud

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Passagem.Data;
 using Passagem.Models;
+using Passagem.ViewModels;
 
 namespace Passagem.Controllers
 {
@@ -12,6 +13,11 @@ namespace Passagem.Controllers
         public DashboardController(AppDbContext db)
         {
             _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
         }
 
         // GET: DashboardController
@@ -27,7 +33,7 @@ namespace Passagem.Controllers
         // GET: DashboardController/Create
         public IActionResult Create()
         {
-            var vm = new NewsViewModel();            
+            var vm = new NewsViewModel();
             vm.ListaCategoria = _db.Categorias.ToList();
 
             return View(vm);
@@ -62,15 +68,15 @@ namespace Passagem.Controllers
         public IActionResult Edit(int? id)
         {
             if (id == null || id == 0)
-                return NotFound();
+                return View("Page404");
 
             var vm = new NewsViewModel();
             vm.ListaCategoria = _db.Categorias.ToList();
 
             var objNews = _db.Noticias.Find(id);
-            
+
             if (objNews == null)
-                return NotFound();
+                return View("Page404");
 
             vm.Noticias = objNews;
 
@@ -106,11 +112,11 @@ namespace Passagem.Controllers
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
-                return NotFound();
+                return View("Page404");
 
             var objNews = _db.Noticias.Find(id);
             if (objNews == null)
-                return NotFound();
+                return View("Page404");
 
             return View(objNews);
         }
@@ -122,7 +128,7 @@ namespace Passagem.Controllers
         {
             var obj = _db.Noticias.Find(id);
             if (obj == null)
-                return NotFound();
+                return View("Page404");
 
             _db.Noticias.Remove(obj);
             _db.SaveChanges();
@@ -130,17 +136,6 @@ namespace Passagem.Controllers
             TempData["success"] = "Notícia deletada com sucesso!";
 
             return RedirectToAction("News");
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [Route("Dashboard/Passagem/Login")]
-        public IActionResult Login()
-        {
-            return View();
         }
 
         [HttpGet]
@@ -154,11 +149,11 @@ namespace Passagem.Controllers
         public IActionResult VisualizarEmail(int? id)
         {
             if (id == null || id == 0)
-                return NotFound();
+                return View("Page404");
 
             var obj = _db.Emails.Find(id);
             if (obj == null)
-                return NotFound();
+                return View("Page404");
 
             return View(obj);
         }
@@ -168,12 +163,12 @@ namespace Passagem.Controllers
         public IActionResult DeleteEmail(int? id)
         {
             if (id == null || id == 0)
-                return NotFound();
+                return View("Page404");
 
             var obj = _db.Emails.Find(id);
 
             if (obj == null)
-                return NotFound();
+                return View("Page404");
 
             _db.Emails.Remove(obj);
             _db.SaveChanges();
@@ -182,5 +177,133 @@ namespace Passagem.Controllers
 
             return RedirectToAction("Emails");
         }
+
+        [Route("Dashboard/Passagem/Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult CreateUser()
+        {
+            var vm = new UserViewModel();
+            vm.ListaCargos = _db.Cargos.ToList();
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser(UserViewModel obj)
+        {
+            ModelState.Remove("User.Cargo");
+
+            var cargoById = _db.Cargos.Find(obj.User.CargoFK);
+
+            if (cargoById != null)
+                obj.User.Cargo = cargoById;
+
+            if (ModelState.IsValid)
+            {
+                _db.Users.Add(obj.User);
+                _db.SaveChanges();
+                TempData["success"] = "Usuário criado com sucesso!";
+
+                return RedirectToAction("Users");
+            }
+
+            return View(obj);
+        }
+        
+        public IActionResult Users()
+        {
+            var vm = new UserListViewModel();
+            vm.ListaUser = _db.Users.ToList();
+            vm.ListaCargo = _db.Cargos.ToList();
+
+            return View(vm);
+        }
+
+        [HttpGet]
+        public IActionResult EditUser(int? id)
+        {
+            if (id == null || id == 0)
+                return View("Page404");
+
+            var vm = new UserViewModel();
+            vm.ListaCargos = _db.Cargos.ToList();
+
+            var objUser = _db.Users.Find(id);
+
+            if (objUser == null)
+                return View("Page404");
+
+            if (objUser.CargoFK == 1)
+                return View("Page404");
+
+            vm.User = objUser;
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditUser(UserViewModel obj)
+        {
+            ModelState.Remove("User.Cargo");
+
+            var cargoById = _db.Cargos.Find(obj.User.CargoFK);
+
+            if (cargoById != null)
+                obj.User.Cargo= cargoById;
+
+            if (ModelState.IsValid)
+            {
+                _db.Users.Update(obj.User);
+                _db.SaveChanges();
+
+                TempData["success"] = "Usuário editado com sucesso!";
+
+                return RedirectToAction("Users");
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult DeleteUser(int? id)
+        {
+            if (id == null || id == 0)
+                return View("Page404");
+
+            var objUser = _db.Users.Find(id);
+            if (objUser == null)
+                return View("Page404");
+
+            if (objUser.CargoFK == 1)
+                return View("Page404");
+
+            return View(objUser);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteUser(int id)
+        {
+            var obj = _db.Users.Find(id);
+            if (obj == null)
+                return View("Page404");
+
+            _db.Users.Remove(obj);
+            _db.SaveChanges();
+
+            TempData["success"] = "Usuário deletado com sucesso!";
+
+            return RedirectToAction("Users");
+        }
+
+        public IActionResult Page404()
+        {
+            return View();
+        }
+
     }
 }
