0e103490741c811fb52b60c17e327fc5f5b73cd4
Marcos <marquinhooliveira9968@outlook.com>
2022-02-07 16:38:15 -0300

    add profile settings

--- Program.cs
+++ Program.cs
@@ -6,14 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
-builder.Services.AddDbContext<PassagemContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("PassagemContextConnection")));
+
+builder.Services.AddDbContext<PassagemContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("PassagemContextConnection")
+    ));
+
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<PassagemContext>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
